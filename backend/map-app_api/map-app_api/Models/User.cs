7fcd96aad18950eea493a4f7dcf11d7d9b9dd93f
Mikuláš Bučko <mikulasbucko1@gmail.com>
2025-07-26 13:05:41 +0200

    Add user model

---
+++ backend/map-app_api/map-app_api/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace map_app_api.Models
+{
+    public class User
+    {
+        public int user_id { get; set; }
+        public required string name { get; set; } 
+
+        // User doesn't have to add their surname
+        public string? surname { get; set; }
+
+        // Change later to standalone tables and add dependencies
+        public int hobby { get; set; }
+        public int travelling_type { get; set; }
+        public int route_type { get; set; }
+        public int companions { get; set; 
+        }
+
+
+    }
+}
