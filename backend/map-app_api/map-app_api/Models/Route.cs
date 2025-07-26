78c66c8b6be3735a4b05e6a9fcca1a0228433dd6
Mikuláš Bučko <mikulasbucko1@gmail.com>
2025-07-26 13:09:21 +0200

    Add Route model

---
+++ backend/map-app_api/map-app_api/Models/Route.cs
@@ -0,0 +1,12 @@
+﻿namespace map_app_api.Models
+{
+    public class Route
+    {
+        public int route_id { get; set; }
+        public required string name { get; set; }
+        public required string location { get; set; }
+        public DateTime from { get; set; }
+        public DateTime to { get; set; }
+
+    }
+}
