1f1b3272828551c5e9d62f89ddbbe387bfc963f1
Mikuláš Bučko <mikulasbucko1@gmail.com>
2025-07-26 13:14:25 +0200

    Add Stop model

---
+++ backend/map-app_api/map-app_api/Models/Stop.cs
@@ -0,0 +1,15 @@
+﻿namespace map_app_api.Models
+{
+
+    // Entity depending on the route
+    public class Stop
+    {
+        public int stop_id { get; set; }
+        public required string name { get; set; }
+        public TimeSpan time_spend { get; set; }
+        public string? fact { get; set; }
+        public string? tip { get; set; }
+
+
+    }
+}
