46a6256f1052f8b6201f4a2ec804090c482cd82a
Mikuláš Bučko <mikulasbucko1@gmail.com>
2025-08-12 13:50:30 +0200

    DBSets for migration

---
+++ backend/map-app_api/map-app_api/Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using map_app_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace map_app_api.Data
+{
+    public class DataContext : DbContext
+    {
+        // Inject DBContextOptions into base DbContext class
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+
+        // Define DBSets for migration and querying
+        public DbSet<Models.Route> Routes { get; set; }
+
+        public DbSet<Stop> Stops { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<RouteTag> RouteTags { get; set; }
+
+        public DbSet<UserRoute> UserRoutes { get; set; }
+
+
+    }
+}
