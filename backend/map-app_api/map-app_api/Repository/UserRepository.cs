d9217fccc9fa38e19ce610861820967bdd3d9a1b
Mikuláš Bučko <mikulasbucko1@gmail.com>
2025-08-16 11:34:31 +0200

    Simple GetUsers method for rp

---
+++ backend/map-app_api/map-app_api/Repository/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using map_app_api.Data;
+using map_app_api.Interfaces;
+using map_app_api.Models;
+
+namespace map_app_api.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+
+        private readonly DataContext m_dataContext;
+
+        public UserRepository(DataContext dataContext)
+        {
+            m_dataContext = dataContext;
+        }
+        
+
+        public ICollection<User> GetUsers()
+        {
+            return m_dataContext.Users.OrderBy(u => u.UserId).ToList();
+        }
+    }
+}
